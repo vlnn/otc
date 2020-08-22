95004e67bbfd59e5e0898672c0c81f30507754af
vlnn <anohin.va@gmail.com>
2020-08-23 02:16:26 +0300

    Init tdd start

--- src/calc/Calc.cs
+++ src/calc/Calc.cs
@@ -5,26 +5,7 @@ using System.Linq;
 namespace calc {
     public class StringCalculator {
         public int Add(string args) {
-            if (IsEmpty(args)) {
                 return 0;
-            }
-            return ParseArgument(args);
-        }
-
-        private int ParseArgument(string input) {
-            if (input.Contains(',')) {
-                return ParseMultiple(input);
-            }
-            return int.Parse(input);
-        }
-        private int ParseMultiple(string input) {
-            string[] parts = input.Split(',');
-            var numbers = parts.Select((number)=>int.Parse(number));
-            return numbers.Sum();
-        }
-
-        private bool IsEmpty(string input) {
-            return input == "";
         }
     }
 }
--- tests/Calc.Tests.cs
+++ tests/Calc.Tests.cs
@@ -5,39 +5,14 @@ using Shouldly;
 namespace test {
     [TestFixture]
     public class Tests {
-        private StringCalculator MakeCalculator() {
-            return new StringCalculator();
-        }
 
         [Test]
         public void Add_EmptyInput_ReturnsZero() {
-            var calc = MakeCalculator();
+            var calc = new StringCalculator();
 
             var zero = calc.Add("");
 
             zero.ShouldBe(0);
-        }
-
-        [TestCase("1", 1)]
-        [TestCase("2", 2)]
-        [TestCase("42", 42)]
-        public void Add_SingleNumber_ReturnsTheNumber(string input, int expected) {
-            var calc = MakeCalculator();
-
-            var result = calc.Add(input);
-
-            result.ShouldBe(expected);
-        }
-
-        [TestCase("1,2",3)]
-        [TestCase("2,2",4)] // 3 nums?
-        [TestCase("40,2",42)]
-        public void Add_MultipleNumbers_ReturnsTheirSum(string input, int expected) {
-            var calc = MakeCalculator();
-
-            var result = calc.Add(input);
-
-            result.ShouldBe(expected);
         }
     }
 }
