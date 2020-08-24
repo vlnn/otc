b7e90aeb893f9d16ac64b81ce2a48acb295440db
vlnn <anohin.va@gmail.com>
2020-08-24 08:38:02 +0300

    Day 5 initial batch
    * src/calc/Calc.cs:

--- src/calc/Calc.cs
+++ src/calc/Calc.cs
@@ -4,11 +4,14 @@ using System.Linq;
 
 namespace calc {
     public class StringCalculator {
+
+        private char delimiter = ',';
+
         public int Add(string args) {
             if (IsNonCalculable(args)){
                 return 0;
                 } else {
-                return GetNumber(args);
+                return GetNumbers(args);
             }
         }
        
@@ -21,11 +24,33 @@ namespace calc {
         }
 
         private bool IsWrongFormat(string input){
-            return !int.TryParse(input, out _);
+            var splittedInput = Split(input);
+            bool[] isValid = {};
+            foreach (string rawNumber in splittedInput){
+                isValid.Append(int.TryParse(rawNumber, out _));
+            }
+            return !isValid.All(x => x);
+        }
+
+        private string[] Split(string input){
+            return input.Split(delimiter);
         }
 
         private int GetNumber(string input){
             return int.Parse(input);
+        }
+
+        private int GetNumbers(string input) {
+            if (input.Contains(delimiter)) {
+                return ParseMultiple(input);
+            }
+            return GetNumber(input);
+        }
+
+        private int ParseMultiple(string input) {
+            string[] parts = Split(input);
+            var numbers = parts.Select((number)=>int.Parse(number));
+            return numbers.Sum();
         }
     }
 }
--- tests/Calc.Tests.cs
+++ tests/Calc.Tests.cs
@@ -2,37 +2,43 @@ using calc;
 using NUnit.Framework;
 using Shouldly;
 
-namespace test {
+namespace test
+{
     [TestFixture]
     public class Tests {
 
+        public StringCalculator SetUp(){
+            return new StringCalculator();
+        }
+
         [Test]
         public void Add_EmptyInput_ReturnsZero() {
-            var calc = new StringCalculator();
+            var calc = SetUp();
 
-            var zero = calc.Add("");
+            var result = calc.Add("");
 
-            zero.ShouldBe(0);
+            result.ShouldBe(0);
         }
 
         [TestCase("0",0)]
         [TestCase("1",1)]
-        [TestCase("42",42)]
         public void Add_IntNumberInput_ReturnsSameNumber(string input, int expected) {
-            var calc = new StringCalculator();
+            var calc = SetUp();
 
             var result = calc.Add(input);
 
             result.ShouldBe(expected);
         }
 
-        [Test]
-        public void Add_Always_ReturnsInt() {
-            var calc = new StringCalculator();
+        [TestCase("1,1",2)]
+        [TestCase("2,1",3)]
+        [TestCase("40,2",42)]
+        public void Add_MultipleNumbers_ReturnsTheirSum(string input, int expected) {
+            var calc = SetUp();
 
-            var result = calc.Add("ABC32");
+            var result = calc.Add(input);
 
-            result.ShouldBe(0);
+            result.ShouldBe(expected);
         }
     }
 }
